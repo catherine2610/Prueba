ba86eeb9999331afee4ce3b7a20fa6d2d38a6f2f
catherine2610 <65379889+catherine2610@users.noreply.github.com>
2021-09-05 23:33:54 -0500

    Add files via upload

---
+++ MODELO SQL.sql
@@ -0,0 +1,34 @@
+﻿---MODELO SQL
+
+CREATE TABLE [DBC].INGREDIENTE(
+
+Ingredientes_Id INTEGER,
+Ingredientes   VARCHAR(100)
+
+) PRIMARY INDEX(Ingredientes_Id);
+
+
+CREATE TABLE [DBC].TRADUCCION(
+
+Traduccion_Id INTEGER,
+Traduccion   VARCHAR(100)
+
+)PRIMARY INDEX(Traduccion_Id);
+
+
+CREATE TABLE [DBC].COMPUESTO(
+
+Compuesto_Id INTEGER,
+Compuesto   VARCHAR(100)
+
+)PRIMARY INDEX(Compuesto_Id);
+
+
+CREATE TABLE [DBC].SINONIMO(
+
+Indicador INTEGER,
+Sinonimo  VARCHAR(100)
+
+)PRIMARY INDEX(Indicador);
+
+
